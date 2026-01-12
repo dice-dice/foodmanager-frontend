722afcb906f028556310b268fa4acb811a42cdfe
disuke <z7daisuke.debzzha@gmail.com>
2026-01-12 21:23:35 +0900

    feat: add providers (Router, QueryClient, Toaster, Auth)

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 import App from './App';
+import { AuthProvider } from './contexts/AuthContext';
 
 // Bootstrap CSS and Icons
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -26,8 +27,10 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
-        <App />
-        <Toaster position="top-right" />
+        <AuthProvider>
+          <App />
+          <Toaster position="top-right" />
+        </AuthProvider>
       </BrowserRouter>
       <ReactQueryDevtools initialIsOpen={false} />
     </QueryClientProvider>
